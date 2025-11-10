012790ca88f3c1460336e3a51adc3b967bd49405
Silvineey <lucreciasilvio@hotmail.com>
2025-11-10 11:21:50 -0300

    ajuste
    ajuste

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function menu(){
     const menu=document.getElementById("submenu")
     menu.classList.toggle("oculto")
 }
+function logar(){
+    alert("Não fiz a opção de logar. Esse botão é só visual.")
+}
